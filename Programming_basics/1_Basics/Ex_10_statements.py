fcb1c1b777624571a066bca0af90b61f793eb133
shubham-ks14 <56676304+shubham-ks14@users.noreply.github.com>
2020-09-03 08:41:51 +0530

    Add files via upload

---
+++ Programming_basics/1_Basics/Ex_10_statements.py
@@ -0,0 +1,31 @@
+s = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9
+print(s)
+n = (1 * 2 * 3 + 7 + 8 + 9)
+print(n)
+footballer = ['MESSI',
+          'NEYMAR',
+          'SUAREZ']
+# Declared using braces {} :
+x = {1 + 2 + 3 + 4 + 5 + 6 +
+     7 + 8 + 9}
+
+#Declared using semicolons(;) :
+flag = 2; ropes = 3; pole = 4
+print(ropes)
+
+#-----------------INDENTATION-------------------
+#python program showing indentation
+site = "sfg"
+
+if site == "sfg":
+    print("Logging into geeksforgeeks")
+else:
+    print("retype the url.")
+print("All set")
+
+j = 1
+while (j <= 5):
+    print(j)
+    j +=1
